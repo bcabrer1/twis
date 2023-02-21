422e021178d1c40429609d674e8cb5e85f4fa3fb
Ben <bcabrer1@terpmail.umd.edu>
2023-02-21 12:57:57 -0500

    adding new features to clone.

--- routes/registerRoutes.js
+++ routes/registerRoutes.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const router = express.Router();
 const bodyParser = require('body-parser')
+const User = require('../schemas/UserSchema')
+const bcrypt = require('bcrypt');
 
 app.set("view engine", "pug");
 app.set("views", "views");
@@ -13,8 +15,8 @@ router.get("/", (req, res, next) => {
     
     res.status(200).render("register");
 })
-
-router.post("/", (req, res, next) => {
+// we use aync in front of function to state async function
+ router.post("/", async (req, res, next) => {
 // trim removes the estra spaces from first name but not in between
 
     var firstName = req.body.firstName.trim();
@@ -25,15 +27,61 @@ router.post("/", (req, res, next) => {
 
     // payload reutrns past entries
 
-    var payLoad = req.body
-    if(firstName && lastName && username && email && password) {
+    // we look for one field of either Username or email 
 
+    var payLoad = req.body
+
+    if(firstName && lastName && username && email && password) {
+        var user = await User.findOne({ 
+            $or: [
+               { username: username },
+               { email : email } 
+            ]
+
+        })
+        .catch((error) => {
+            console.log(error);
+
+            payload.errorMessage = "somethigns went really wrong";
+            res.status(200).render("register", payload);
+
+       })
     
     }
     else {
 
-        payLoad.console.error(Message = " Mae sure eaxh field has a vali value");
+        payload.errorMessage = "Mae sure eaxh field has a vali value";
         res.status(200).render("register", payLoad);
+    }
+
+    if(user == null) {
+        // no user found
+
+        var data = req.body
+
+        // we hash our password with imported library
+        // we salt the rounf to add reinforcmetn 10X
+
+        data.password = await bcrypt.hash(password, 10)
+
+
+
+        // we create use in database video 37
+        User.create(data)
+        .then((user) => {
+            console.log(user);
+        })
+
+    }
+    else {
+        // User found
+        if (email == user.email) {
+            payload.errorMessage = "Email alreadu in use"; 
+        }
+        else {
+            payload.errorMessage = " USername already in use";
+        }
+        res.status(200).render("register", payload);
     }
 
 })
---
+++ schemas/UserSchema.js
@@ -0,0 +1,28 @@
+  const mongoose = require('mongoose');
+  
+
+
+  const Schema = mongoose.Schema;
+
+  const UserSchema = new Schema({
+    firstName: {
+      type: String,
+      required: true,
+      trim: true
+    },
+    lastName: {
+      type: String,
+      required: true,
+       trim: true 
+
+    },
+    userName: { type: String, required: true, trim: true, unique: true },
+    password: { type: String, required: true},
+    profilePic: { type: String, default: "/images/profilePic.png" },
+  },
+  // timestamps will shows updated timestamps
+  { timestamps: true }); 
+
+
+  var User = mongoose.model('User', UserSchema);
+  module.exports = User;
